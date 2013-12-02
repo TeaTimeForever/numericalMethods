3d95d3db518d83c45c5bc6e84bd3069a48888363
Karina Pilyushonoka <k.pilyushonoka@gmail.com>
2013-12-02 10:56:46 +0200

    start thinking about simple iterations method

---
+++ numericalMoethods_1st/simpleIterations.js
@@ -0,0 +1,12 @@
+
+var a = 0.1;
+var b = 1;
+var e = 0.0001;
+
+console.log(simpleIt(a, b, e))
+
+function f(x) {
+	return x / (Math.pow(Math.sin(3*x), 2)) - 17// my homework
+}
+
+function simpleIt(a, b, e) {}
