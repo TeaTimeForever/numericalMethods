a873d672324e4a65b7253438ec61bda7d074a126
Karina Pilyushonoka <k.pilyushonoka@gmail.com>
2013-12-04 11:46:27 +0200

    lala

--- numericalMoethods_1st/simpleIterations.js
+++ numericalMoethods_1st/simpleIterations.js
@@ -9,4 +9,18 @@ function f(x) {
 	return x / (Math.pow(Math.sin(3*x), 2)) - 17// my homework
 }
 
-function simpleIt(a, b, e) {}
+function simpleIt(a, b, e) {
+	var x = (a+b)/2
+	var next = f(x)
+	var i = 0
+	
+	console.log('it: ' + i + ' x: ' + x + 'next: ' + next)
+	i++
+	while (Math.abs(next - x) > e) {
+		x = next
+		next = f(x)
+		i++
+		console.log('it: ' + i + ' x: ' + x + ' next: ' + next)
+	}
+	
+}
