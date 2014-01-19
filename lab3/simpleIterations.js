e591f1b03a3b377fcc37e4481682a08b2efe429e
Karina Pilyushonoka <k.pilyushonoka@gmail.com>
2014-01-19 21:13:07 +0200

    final

--- lab3/simpleIterations.js
+++ lab3/simpleIterations.js
@@ -10,8 +10,8 @@ function df(x) {
 	return (1 - 6*x/ Math.tan(3*x)) * (1/Math.pow(Math.sin(3*x), 2)) // my homework
 }
 
-function dF(x) {
-	return df(x) + 1
+function dF(x, k) {
+	return 1- k* df(x) 
 }
 
 function t(x) {
@@ -20,51 +20,30 @@ function t(x) {
 
 function simpleIt(a, b, e, k) {
 	var x = (a+b)/2
-//	var k = 0.05
-	var next = x - k * f(x) 
+	var next =x - f(x)
+	console.log('f(x):' + f(x))
+	console.log(dF(next, k))
+	next =x -  k * f(x)
+	console.log('next:' + f(x))
+	console.log(dF(next, k))
 	var i = 1
-	
-	console.log('x: ' + next + ' ->  ' + dF(next))
 	
 	while (Math.abs(next - x) > e) {
 		x = next
-		next = x - k * f(x)
+		next =x -  k * f(x) 
 		i++
-		//console.log('x: ' + next + ' ->  ' + dF(next))
 	}
 	return next
 }
-var ks = [
--0.056, 
--0.057, 
--0.058, 
--0.059, 
--0.122, 
--0.123, 
--0.124, 
--0.187, 
--0.188, 
--0.251, 
--0.252, 
--0.316, 
--0.379, 
--0.443, 
--0.507, 
--0.571, 
--0.698, 
--0.762, 
--0.953, 
-]
 
-for(var i=0; i<ks.length; i++) {
-	console.log(' ')
-	console.log(simpleIt(a, b, e, ks[i]))
-}
-//console.log(simpleIt(a, b, e))
-//for (var i=0; i>-1; i-=0.001) {
+
+
+console.log(simpleIt(a,b,e,0.003))
+console.log('[')
+//for (var i=-1; i<1; i+=0.001) {
 //	var x = 0.55
 //	x = i * f(x) + x
-//	
-//	if(Math.abs(dF(x)) < 1) console.log(i + ', ')
+//	if(Math.abs(dF(x, i)) < 1) console.log(i.toFixed(3) + ', ')
 //	
 //}
+console.log(']')
